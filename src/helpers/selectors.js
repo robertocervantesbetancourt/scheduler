84b4fd77a0f677dbe6da335d3e4747eca986fdc5
Roberto Cervantes <robertocervantes@Robertos-MBP-2.phub.net.cable.rogers.com>
2022-09-14 18:20:59 -0400

    Create getAppointmentsForDay selector and test

---
+++ src/helpers/selectors.js
@@ -0,0 +1,31 @@
+export function getAppointmentsForDay(state, day){
+  let appointments;
+  let appointmentList=[];
+
+  if(state.days.length === 0){
+    return appointmentList
+  }
+
+  for(let d of state.days){
+    if(d.name === day){
+      appointments = d.appointments;
+    }
+  }
+
+  if(!appointments){
+    return appointmentList
+  }
+
+  for(let appointment of appointments){
+      appointmentList.push(state.appointments[appointment])
+    
+  }
+  console.log(appointments)
+  return appointmentList
+}
+
+
+
+
+
+
