ef1b2ba9be1652a11330b82585bc63fad4726f89
Roberto Cervantes <robertocervantes@Robertos-MBP-2.phub.net.cable.rogers.com>
2022-09-13 22:09:19 -0400

    Add .get api/days to application.js

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "components/Appointment";
@@ -43,29 +44,37 @@ const appointments = {
 };
 
 export default function Application(props) {
-  const days = [
-    {
-      id: 1,
-      name: "Monday",
-      spots: 2,
-    },
-    {
-      id: 2,
-      name: "Tuesday",
-      spots: 5,
-    },
-    {
-      id: 3,
-      name: "Wednesday",
-      spots: 0,
-    },
-  ];
+
+  const [day, setDay] = useState("Monday");
+  const [days, setDays] = useState([]);
+
+  useEffect(() => {
+    axios.get('/api/days')
+    .then(response => setDays(response.data))
+  })
+
+  // const days = [
+  //   {
+  //     id: 1,
+  //     name: "Monday",
+  //     spots: 2,
+  //   },
+  //   {
+  //     id: 2,
+  //     name: "Tuesday",
+  //     spots: 5,
+  //   },
+  //   {
+  //     id: 3,
+  //     name: "Wednesday",
+  //     spots: 0,
+  //   },
+  // ];
 
   const appointment = Object.values(appointments).map((appointment) => (
     <Appointment key={appointment.id} {...appointment}/>
   ));
 
-  const [day, setDay] = useState("Monday");
 
   return (
     <main className="layout">
