cde16487921d04ab7fdc6029306899b408b55375
Roberto Cervantes <robertocervantes@Robertos-MBP-2.phub.net.cable.rogers.com>
2022-09-15 15:04:45 -0400

    implement custom hook for back and transition states

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,31 @@
+import { useState } from "react";
+
+export default function useVisualMode(initial) {
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
+
+  function transition(newMode, replace = false) {
+    if(replace){
+      setMode(newMode);
+      setHistory((prev) => [...prev]);
+    } else {
+      setMode(newMode);
+      setHistory((prev) => [...prev, newMode]);
+    }
+  }
+
+  function back() {
+    if (history.length > 1) {
+      history.pop();
+      setMode(history[history.length - 1]);
+    } else {
+      mode
+    }
+  }
+
+  return {
+    mode,
+    transition,
+    back,
+  };
+}
