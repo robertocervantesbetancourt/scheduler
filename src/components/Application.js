8967198a461c441c855e172d8e56d7d96e34e710
Roberto Cervantes <robertocervantes@Robertos-MBP-2.phub.net.cable.rogers.com>
2022-09-14 20:46:17 -0400

    implement Promise.all to combine days and appointments calls in one object

--- src/components/Application.js
+++ src/components/Application.js
@@ -3,6 +3,7 @@ import axios from "axios";
 import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "components/Appointment";
+import { getAppointmentsForDay } from "helpers/selectors";
 
 // const appointments = {
 //   1: {
@@ -51,17 +52,21 @@ export default function Application(props) {
   });
 
   const setDay = (day) => setState({ ...state, day });
-  //const setDays = (days) => setState(prev => ({...prev, days }));
 
-  const dailyAppointments = [];
+  let dailyAppointments = [];
 
   useEffect(() => {
     Promise.all([axios.get("/api/days"), axios.get("/api/appointments")]).then(
       (all) => {
         setState((prev) => ({ ...prev, days: all[0].data, appointments: all[1].data }));
       }
-    );
-  }, []);
+      );
+    }, []);
+
+    console.log(state)
+
+  dailyAppointments = getAppointmentsForDay(state, state.day)
+
 
   const appointment = dailyAppointments.map((appointment) => (
     <Appointment key={appointment.id} {...appointment} />
