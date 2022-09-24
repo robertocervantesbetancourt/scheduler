f6375e02a0b44923d8b66891ed3da1f000b5ffb4
Roberto Cervantes <robertocervantes@Robertos-MBP-2.phub.net.cable.rogers.com>
2022-09-24 15:29:30 -0400

    Add saving status to appointment

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -4,11 +4,13 @@ import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
 import Form from "./Form";
+import Status from "./Status";
 import useVisualMode from "hooks/useVisualMode";
 
 const EMPTY = "EMPTY";
 const SHOW = "SHOW";
 const CREATE = "CREATE";
+const SAVING = "SAVING";
 
 export default function Appointment(props) {
   const { mode, transition, back } = useVisualMode(
@@ -20,6 +22,7 @@ export default function Appointment(props) {
       student: name,
       interviewer,
     }
+    transition(SAVING)
     props
       .bookInterview(props.id, interview)
       .then(() => transition(SHOW))
@@ -43,6 +46,7 @@ export default function Appointment(props) {
           onSave={save}
         />
       )}
+      {mode === SAVING && <Status message={'SAVING...'}/>}
     </article>
   );
 }
