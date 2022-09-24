6e86911dae4032076de38f8ebdde484e10aceb44
Roberto Cervantes <robertocervantes@Robertos-MBP-2.phub.net.cable.rogers.com>
2022-09-24 17:18:21 -0400

    Implement error state in appointment

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -6,6 +6,7 @@ import Empty from "./Empty";
 import Form from "./Form";
 import Status from "./Status";
 import Confirm from "./Confirm";
+import Error from "./Error"
 import useVisualMode from "hooks/useVisualMode";
 
 const EMPTY = "EMPTY";
@@ -15,6 +16,8 @@ const SAVING = "SAVING";
 const DELETE = "DELETE";
 const CONFIRM = "CONFIRM";
 const EDIT = "EDIT";
+const ERROR_SAVE = "ERROR_SAVE"
+const ERROR_DELETE = "ERROR_DELETE"
 
 export default function Appointment(props) {
   const { mode, transition, back } = useVisualMode(
@@ -27,12 +30,17 @@ export default function Appointment(props) {
       interviewer,
     };
     transition(SAVING);
-    props.bookInterview(props.id, interview).then(() => transition(SHOW));
+    props
+      .bookInterview(props.id, interview)
+      .then(() => transition(SHOW))
+      .catch(() => transition(ERROR_SAVE, true))
   }
 
   function deleteAppointment() {
     transition(DELETE, true);
-    props.cancelInterview(props.id).then(() => transition(EMPTY));
+    props.cancelInterview(props.id)
+      .then(() => transition(EMPTY))
+      .catch(() => transition(ERROR_DELETE, true))
   }
 
   return (
@@ -68,6 +76,8 @@ export default function Appointment(props) {
           onSave={save}
         />
       )}
+      {mode === ERROR_DELETE && <Error message={'Error deleting'} onClose={back}/>}
+      {mode === ERROR_SAVE && <Error message={'Error saving'} onClose={back}/>}
     </article>
   );
 }
