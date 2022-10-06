5774986b27adf276da64d82c443655321218bb10
Roberto Cervantes <robertocervantes@Robertos-MBP-2.phub.net.cable.rogers.com>
2022-10-05 23:46:26 -0400

    Add test to Check day:Monday and no spots remaining

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -9,6 +9,7 @@ import {
   getAllByTestId,
   getByAltText,
   getByPlaceholderText,
+  queryByText,
 } from "@testing-library/react";
 
 import { fireEvent } from "@testing-library/react/dist";
@@ -28,19 +29,31 @@ describe("Application", () => {
   });
 
   it("loads data, books and interview and reduces the spots remaining for the first day by 1", async () => {
-    const { container } = render(<Application />);
-    
+    const { container, debug } = render(<Application />);
+
     await waitForElement(() => getByText(container, "Archie Cohen"));
+
     const appointments = getAllByTestId(container, "appointment");
-    const appointment = appointments[0]
+    const appointment = appointments[0];
+
     fireEvent.click(getByAltText(appointment, "Add"));
-    
+
     fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
       target: { value: "Lydia Miller-Jones" },
     });
+
     fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
-    
+
     fireEvent.click(getByText(appointment, "Save"));
-    console.log(prettyDOM(appointment));
+    
+    expect(getByText(appointment, "SAVING...")).toBeInTheDocument();
+    
+    await waitForElement(() => queryByText(appointment, "Lydia Miller-Jones"));
+    
+    const day = getAllByTestId(container, "day").find((day) =>
+      queryByText(day, "Monday")
+    );
+
+    expect(getByText(day, "no spots remaining")).toBeInTheDocument();
   });
 });
